f7611643e522e1ed23b0a7ece29878929e8e132a
kashi <24215309+kashi03@users.noreply.github.com>
2021-05-18 10:49:59 +0900

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
 import typing
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 @dataclass
 class State():
@@ -23,11 +23,27 @@ class State():
     def __ge__(self, o) -> bool:
         return self.combo >= o.combo
 
-def max_commbo(nums):
-    pass
-
+def beam_search(first_state, move_number):
+    now_states = [] # 現在の状態保存用
+    heapq.heappush(now_states, first_state)
+    k = 1000 #ビーム幅
+    
+    for _ in range(move_number):
+        next_states = []
+        for state in now_states:
+            pass
 
 def main():
-    pass
+    field = [
+        [0,1,2,3,4,5,6],
+        [1,2,3,4,5,6,0],
+        [2,3,4,5,6,0,1],
+        [3,4,5,6,0,1,2],
+        [4,5,6,0,1,2,3]
+    ]
+    
+    first_state = State(field, 0, (0, 0))
+    beam_search(first_state, 100)
+
 if __name__ == "__main__":
     main()
