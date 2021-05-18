e83d8a6feb9b8fc3992b890a5f710fa26a537cf0
kashi <24215309+kashi03@users.noreply.github.com>
2021-05-18 15:27:42 +0900

    update: Pointデータクラスを追加

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import typing
 from dataclasses import dataclass
 
 @dataclass
+class Point:
+    x: int
+    y: int
+
+@dataclass
 class State():
     field: typing.List[typing.List[int]]
     combo: int
-    point: typing.Tuple[int, int]
-    move_history: typing.List[typing.Tuple[int, int]]
+    point: Point
+    move_history: typing.List[Point]
 
     def __eq__(self, o) -> bool:
         return self.combo == o.combo
@@ -24,6 +29,10 @@ class State():
     def __ge__(self, o) -> bool:
         return self.combo >= o.combo
 
+def move_drop(f, src_point: typing.Tuple, dst_pint: typing.Tuple):
+    field = [ [ i for i in j ] for j in f ]
+    field[src_point[1]]
+
 def beam_search(first_state, move_number):
     now_states = [] # 現在の状態保存用
     heapq.heappush(now_states, first_state)
@@ -31,8 +40,9 @@ def beam_search(first_state, move_number):
     
     for _ in range(move_number):
         next_states = []
-        for state in now_states:
-            pass
+        for _ in range(k):
+            state = heapq.heappop(now_states)
+            
 
 def main():
     field = [
@@ -43,7 +53,8 @@ def main():
         [4,5,6,0,1,2,3]
     ]
     
-    first_state = State(field, 0, (0, 0), [(0, 0)])
+    first_point = Point(0, 0)
+    first_state = State(field, 0, first_point, [first_point])
     beam_search(first_state, 100)
 
 if __name__ == "__main__":
