d1d28852a1c1b54d8f175c08c18c35271adfe219
kashi <24215309+kashi03@users.noreply.github.com>
2021-05-18 16:02:42 +0900

    update: コンボ解析の実装

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import heapq
 import typing
 from dataclasses import dataclass
 
+from util.puzzle import Puzzle
+
 @dataclass
 class Point:
     x: int
@@ -29,20 +31,36 @@ class State():
     def __ge__(self, o) -> bool:
         return self.combo >= o.combo
 
-def move_drop(f, src_point: typing.Tuple, dst_pint: typing.Tuple):
-    field = [ [ i for i in j ] for j in f ]
-    field[src_point[1]]
+def get_next_state(state: State, dst: Point):
+    field = [ [ i for i in j ] for j in state.field ]
+    field[state.point.y][state.point.x], field[dst.y][dst.x] = field[dst.y][dst.x], field[state.point.y][state.point.x]
+    combo = Puzzle(field).get_combo() * -1 # heap1は最小値が取り出されるため-1を乗算
+    move_history = state.move_history + [dst]
+    next_state = State(field, combo, dst, move_history)
+    return next_state
 
 def beam_search(first_state, move_number):
+    field_width = len(first_state.field[0])
+    field_height = len(first_state.field)
     now_states = [] # 現在の状態保存用
     heapq.heappush(now_states, first_state)
-    k = 1000 #ビーム幅
-    
+    k = 5 #ビーム幅
+    comb = set()
     for _ in range(move_number):
+        print(len(now_states))
         next_states = []
         for _ in range(k):
+            if len(now_states) == 0: break
             state = heapq.heappop(now_states)
-            
+            comb.add(state.combo)
+            if state.point.x-1 >= 0: next_states.append( get_next_state(state, Point( state.point.x-1, state.point.y )) )
+            if state.point.x+1 < field_width: next_states.append( get_next_state(state, Point( state.point.x+1, state.point.y )) )
+            if state.point.y-1 >= 0: next_states.append( get_next_state(state, Point( state.point.x, state.point.y-1 )) )
+            if state.point.y+1 < field_height: next_states.append( get_next_state(state, Point( state.point.x, state.point.y+1 )) )
+        heapq.heapify(next_states)
+        now_states = next_states
+    print(comb)
+    return heapq.heappop(now_states)
 
 def main():
     field = [
@@ -53,9 +71,10 @@ def main():
         [4,5,6,0,1,2,3]
     ]
     
-    first_point = Point(0, 0)
+    first_point = Point(2, 2)
     first_state = State(field, 0, first_point, [first_point])
-    beam_search(first_state, 100)
+    result = beam_search(first_state, 20)
+    print(result)
 
 if __name__ == "__main__":
     main()
